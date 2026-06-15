7ad67b80d8d00a1e424dce3e07fc3164e6739aac
JeanPi-Kendo <jeanpierreroahernandez@gmail.com>
2026-06-15 18:01:06 -0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+from app import create_app, db
+from app.models.user import User
+from app.models.progress import Progress
+
+
+app = create_app()
+
+
+with app.app_context():
+
+    db.create_all()
+
+
+if __name__ == "__main__":
+
+    app.run(
+        debug=True
+    )
